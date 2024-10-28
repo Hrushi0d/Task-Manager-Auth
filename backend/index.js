abe0f35d700f58af7c38e88611c6d89bfbfa2b13
unknown <hrushi0d@gmail.com>
2024-10-28 13:15:01 +0530

    Express and Nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
+import express from "express";
+import {PORT} from './config.js';
 
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`app listening at ${PORT}`);
+});
+
+app.get("/")
