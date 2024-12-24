eb3008c5a8f63a3c9772be304cb59942fdd8e37b
unknown <hrushi0d@gmail.com>
2024-12-24 14:08:26 +0530

    Deleted Old backend

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-
-export const PORT= 5555;
-export const MongoDBURL = 'mongodb+srv://root:root@tasks.qkozv.mongodb.net/?retryWrites=true&w=majority&appName=Tasks';
--- backend/models/usersmodel.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Model, Types } from "mongoose";
-
-const userSchema = new mongoose.Schema({
-    name:{type:String, required:true,},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
-});
-
-export const user = mongoose.model('Users', userSchema);
